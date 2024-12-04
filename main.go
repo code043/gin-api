68880727407f37affb6f08a720f510d3b64ab8cf
code043 <procoder043@gmail.com>
2024-12-04 16:45:00 -0300

    Route test

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	router := *gin.Default()
-	router.Run(":8080")
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"msg": "Gin",
+		})
+	})
+	router.Run(":3000")
 }
