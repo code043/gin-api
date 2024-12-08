14b3d41cce404e375c5bf07e6e1e110a4eca49f5
code043 <procoder043@gmail.com>
2024-12-08 18:16:43 -0300

    Setting routes

--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,6 @@ func Routes(r *gin.Engine, noteHandler *handlers.NoteHandler) {
 	r.POST("/notes", noteHandler.Create)
 	r.GET("/notes", noteHandler.GetAll)
 	r.GET("/notes/:id", noteHandler.GetByID)
+	r.PUT("/notes/:id", noteHandler.Update)
 	r.DELETE("/notes/:id", noteHandler.Delete)
 }
